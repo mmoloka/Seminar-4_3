ef99a9db26c72c54fba6b742852f3672d4ddafb1
«Mikhail» <kopfgolova@gmail.com>
2022-05-16 21:42:05 +0300

    add Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/
+int GetNumber(string msg)
+{
+while(true)
+{
+Console.WriteLine(msg);
+string valueFromConsole = Console.ReadLine();
+
+    if(int.TryParse(valueFromConsole, out int number))
+    {
+        return number;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не число. Нужно ввести число.");
+    }
+}
+}
+double GetLengthVector(double x1, double y1, double x2, double y2)
+{
+    double difx = Math.Pow((x1-x2), 2);
+    double dify = Math.Pow((y1-y2), 2);
+    double length = Math.Sqrt(difx + dify);
+    return length;
+}
+int x1 = GetNumber("Введите координату точки A по оси x:");
+int y1 = GetNumber("Введите координату точки A по оси y:");
+int x2 = GetNumber("Введите координату точки B по оси x:");
+int y2 = GetNumber("Введите координату точки B по оси y:");
+
+double result = GetLengthVector( x1,  y1,  x2,  y2);
+Console.WriteLine(result);
